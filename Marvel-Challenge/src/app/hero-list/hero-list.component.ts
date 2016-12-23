024f3ac93b37d8b7762a42182b7b33daea6e18e4
Juan David Insuasti Bravo <juan.insuasti@gmail.com>
2016-12-22 23:17:02 -0500

    HTTP Service refactor and injection in hero-list component
    baseline http service refactor using URLSearchParameters class.

--- Marvel-Challenge/src/app/hero-list/hero-list.component.ts
+++ Marvel-Challenge/src/app/hero-list/hero-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-hero-list',
   templateUrl: './hero-list.component.html',
-  styleUrls: ['./hero-list.component.css']
+  styleUrls: ['./hero-list.component.css'],
+  providers: [HttpService]
 })
 export class HeroListComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(private httpService: HttpService) {
+  }
 
   ngOnInit() {
+      this.httpService.getData('Iron', 'name').subscribe(
+        (data: Response) => console.log(data.json())
+          );
+  }
+
+  getHeroes(){
+
   }
 
 }
--- Marvel-Challenge/src/app/http.service.ts
+++ Marvel-Challenge/src/app/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from "@angular/http";
+import { URLSearchParams, Http } from "@angular/http";
 
 @Injectable()
 export class HttpService {
@@ -7,13 +7,15 @@ export class HttpService {
   constructor(private http:Http) { }
 
   getData(searchInput:string, orderBy:string){
-    let params = ``;
-    if( searchInput !== '' )
-        params += `nameStartsWith=${searchInput}&`;
-
-    params += `orderBy=${orderBy}`;
-
-    return this.http.get(`https://gateway.marvel.com//v1/public/characters?apikey=6e8fd6170783e1ecf8c0e3784c8c00f2&hash=effdd68f568a43ae712240c3ce880e86&ts=1&limit=10&${params}`);
+    const url = "https://gateway.marvel.com//v1/public/characters";
+    let params = new URLSearchParams();
+    params.set('hash', 'effdd68f568a43ae712240c3ce880e86');
+    params.set('apikey', '6e8fd6170783e1ecf8c0e3784c8c00f2');
+    params.set('ts', '1');
+    params.set('limit', '10');
+    if(searchInput) params.set('nameStartsWith', searchInput)
+    if(orderBy) params.set('orderBy', orderBy);
+    return this.http.get(url, {search: params});
   }
 
 }
