a5796f468e55bd87fc5a76561316f6f1fba2aef4
Juan David Insuasti Bravo <juan.insuasti@gmail.com>
2017-01-04 00:05:54 -0500

    HTTP Service update
    Added getCharacterComicData method to get all the comics for a given character, this will be used in the hero-list character modal.
    Added getComicData method to get comic data for a single comic, will be used in the add to favorites modal.

--- Marvel-Challenge/src/app/http.service.ts
+++ Marvel-Challenge/src/app/http.service.ts
@@ -14,9 +14,28 @@ export class HttpService {
     params.set('ts', '1');
     params.set('limit', '10');
     params.set('offset', String(offset));
-    if(searchInput) params.set('nameStartsWith', searchInput)
+    if(searchInput) params.set('nameStartsWith', searchInput);
     if(orderBy) params.set('orderBy', orderBy);
     return this.http.get(url, {search: params});
   }
 
+  getCharacterComicData(charId:string, orderBy:string = "title", offset:number=0){
+    const url = `https://gateway.marvel.com//v1/public/characters/${charId}/comics`;
+    let params = new URLSearchParams();
+    params.set('hash', 'effdd68f568a43ae712240c3ce880e86');
+    params.set('apikey', '6e8fd6170783e1ecf8c0e3784c8c00f2');
+    params.set('ts', '1');
+    params.set('offset', String(offset));
+    if(orderBy) params.set('orderBy', orderBy);
+    return this.http.get(url, {search: params});
+  }
+
+  getComicData(comicId:string){
+    const url = `https://gateway.marvel.com//v1/public/comics/${comicId}`;
+    let params = new URLSearchParams();
+    params.set('hash', 'effdd68f568a43ae712240c3ce880e86');
+    params.set('apikey', '6e8fd6170783e1ecf8c0e3784c8c00f2');
+    params.set('ts', '1');
+    return this.http.get(url, {search: params});
+  }
 }
