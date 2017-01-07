a8c10c9c7801d89d02cf7a2de330c84722b2c8ce
Juan David Insuasti Bravo <juan.insuasti@gmail.com>
2017-01-07 08:34:09 -0500

    cleanup temp.ts

--- Temp.ts
+++
@@ -1,8 +0,0 @@
-{
-              'name': object.name,
-              'id': object.id,
-              'thumbnail': `${object.thumbnail.path}.${object.thumbnail.extension}`,
-              'comics': object.comics.items,
-              'description': object.description,
-              'comicList': []
-            };
