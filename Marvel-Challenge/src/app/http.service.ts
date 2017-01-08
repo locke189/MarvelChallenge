824276504495e5b39f29f446c183fbcbdc667737
Juan David Insuasti Bravo <juan.insuasti@gmail.com>
2017-01-07 19:41:43 -0500

    Http service... change http for https if needed.

--- Marvel-Challenge/src/app/http.service.ts
+++ Marvel-Challenge/src/app/http.service.ts
@@ -41,6 +41,11 @@ export class HttpService {
 
   getComicDataFromUrl(url:string){
     let params = new URLSearchParams();
+
+    if( url.search('https') === -1 ){
+      url = url.replace("http","https");
+    }
+
     params.set('hash', 'effdd68f568a43ae712240c3ce880e86');
     params.set('apikey', '6e8fd6170783e1ecf8c0e3784c8c00f2');
     params.set('ts', '1');
