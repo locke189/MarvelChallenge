a4efe2cebf1bf4aeec72f2aacebe4c4ec57578b0
Juan David Insuasti Bravo <juan.insuasti@gmail.com>
2017-01-09 21:13:49 -0500

    Responsive header

--- Marvel-Challenge/src/app/header/header.component.ts
+++ Marvel-Challenge/src/app/header/header.component.ts
@@ -9,7 +9,7 @@ export class HeaderComponent implements OnInit {
 
   @Output() searchTerm = new EventEmitter();
   @Output() mainContent = new EventEmitter();
-
+  isCharacter: boolean = true;
   constructor() { }
 
   ngOnInit() {
@@ -22,8 +22,12 @@ export class HeaderComponent implements OnInit {
   }
 
   viewInMain(component:string){
+    if(component === "characters"){
+      this.isCharacter = true;
+    } else {
+      this.isCharacter = false;
+    }
     this.mainContent.emit(component);
-    document.getElementById("navbar-toggle-button").click();
   }
 
 
